554c750e1bac1333d8e9ac783cc4506ec3564fa7
andrzejfidut <59800897+andrzejfidut@users.noreply.github.com>
2023-12-18 16:27:55 +0100

    commit day2 zad 1

---
+++ src/main/java/com/andrzejfidut/java/basic/exercies/day2/Task1.java
@@ -0,0 +1,27 @@
+package com.andrzejfidut.java.basic.exercies.day2;
+
+
+    public class Task1 {
+        //    Napisz program, który przyjmie jako parametr tekst (typu String), a następnie zwróci jego
+//    ostatni znak.
+//    Przykład:
+//    Original word: ala
+//    Last Char: a
+//    Original word: domek
+//    Last Char: k
+        public static void main(String[] args) {
+
+            System.out.println("Original word: ala");
+            System.out.println("Last Char: " + getLastChar("ala"));
+
+            System.out.println("Original word: domek");
+            System.out.println("Last Char: " + getLastChar("domek"));
+        }
+        private static char getLastChar(String text) {
+            if (text != null && !text.isEmpty()) {
+                return text.charAt(text.length() - 1);
+            } else {
+                return 0;
+            }
+        }
+    }
