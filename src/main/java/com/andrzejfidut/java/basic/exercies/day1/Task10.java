fb757508f921c77ac582d76de1f9271072bda342
andrzejfidut <59800897+andrzejfidut@users.noreply.github.com>
2023-12-18 16:00:55 +0100

    commit day1 home work zad 10

---
+++ src/main/java/com/andrzejfidut/java/basic/exercies/day1/Task10.java
@@ -0,0 +1,32 @@
+package com.andrzejfidut.java.basic.exercies.day1;
+
+import java.util.Scanner;
+
+
+    // Napisz program, który pobierze od użytkownika liczbę całkowitą (typu int), a następnie obliczy
+//         sumę cyfr podanej liczby.
+    public class Task10 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                System.out.print("Please insert digit: ");
+                int number = scanner.nextInt();
+
+                int result = calculateTheSumOfTheGigits (number);
+                System.out.println("Sum Of the digits: " + result);
+                break;
+            }
+        }
+
+        static int calculateTheSumOfTheGigits(int number) {
+
+            int result = 0;
+
+            while (number != 0) {
+                result += number % 10;
+                number /= 10;
+            }
+            return result;
+        }
+}
