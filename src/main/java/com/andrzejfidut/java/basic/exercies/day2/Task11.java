e3ed1c62d9a457ff6dfd1c8e0fab8402357c62e5
andrzejfidut <59800897+andrzejfidut@users.noreply.github.com>
2023-12-18 16:43:47 +0100

    commit day2 zad 11

---
+++ src/main/java/com/andrzejfidut/java/basic/exercies/day2/Task11.java
@@ -0,0 +1,31 @@
+package com.andrzejfidut.java.basic.exercies.day2;
+
+import java.util.Scanner;
+
+    public class Task11 {
+        //    Napisz program, który będzie sprawdzał czy podane słowo (typu String) jest palindromem.
+//    Przykład:
+//    Input: kajak
+//    Result: true
+//    Input: sda
+//    Result: false
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Input: ");
+            String word = scanner.nextLine();
+
+            boolean isPalindrome = checkPalindrome(word);
+
+            System.out.println("Result: " + isPalindrome);
+        }
+        private static boolean checkPalindrome(String word) {
+            // Usuń białe znaki i zamień na małe litery
+            String cleanedWord = word.replaceAll("\\s", "").toLowerCase();
+
+            // Porównaj słowo z jego odwróconą wersją
+            String reversedWord = new StringBuilder(cleanedWord).reverse().toString();
+            return cleanedWord.equals(reversedWord);
+        }
+    }
