206c53ed8725bf831f813c7aec5975bfb1bb7cd0
andrzejfidut <59800897+andrzejfidut@users.noreply.github.com>
2023-12-18 16:41:25 +0100

    commit day2 zad 8

---
+++ src/main/java/com/andrzejfidut/java/basic/exercies/day2/Task8.java
@@ -0,0 +1,37 @@
+package com.andrzejfidut.java.basic.exercies.day2;
+
+import java.util.Scanner;
+
+    public class Task8 {
+//    Napisz program, który przyjmie jako argument tekst (typu String), a następnie zamieni małe
+//    litery na duże i odwrotnie, w podanym łańcuchu wejściowym.
+//    Przykład:
+//    Input: AlaKot
+//    Result: aLAkOT
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Please insert your text: ");
+            String userInput = scanner.nextLine();
+
+            String result = reverseCase(userInput);
+
+            System.out.println("Result: " + result);
+        }
+        private static String reverseCase(String text) {
+            StringBuilder result = new StringBuilder();
+
+            for (char c : text.toCharArray()) {
+                if (Character.isUpperCase(c)) {
+                    result.append(Character.toLowerCase(c));
+                } else if (Character.isLowerCase(c)) {
+                    result.append(Character.toUpperCase(c));
+                } else {
+                    result.append(c);
+                }
+            }
+            return result.toString();
+        }
+    }
