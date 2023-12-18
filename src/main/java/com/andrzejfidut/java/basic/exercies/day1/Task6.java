624885745026c525cec8c43fa5bf6c5c6fa9285b
andrzejfidut <59800897+andrzejfidut@users.noreply.github.com>
2023-12-18 15:55:00 +0100

    commit day1 home work zad 6

---
+++ src/main/java/com/andrzejfidut/java/basic/exercies/day1/Task6.java
@@ -0,0 +1,18 @@
+package com.andrzejfidut.java.basic.exercies.day1;
+
+
+    public class Task6 {
+        public static void main(String[] args) {
+
+//    Metoda do drikowania tabliczki mnożenia
+
+            printMultiplicationTable (5,1,10);
+        }
+        //    Metoda do wydrukowania fragmentu tabliczki mnożenia
+        static void printMultiplicationTable(int multiplier, int minMultiplicand, int maxMultiplicand) {
+            for (int multiplicand = minMultiplicand; multiplicand <= maxMultiplicand; multiplicand++) {
+                int result = multiplier * multiplicand;
+                System.out.println (multiplier + " * " + multiplicand + " = " + result);
+            }
+        }
+    }
